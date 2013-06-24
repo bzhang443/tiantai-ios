6f03805b23f9e7dc23e31087227d3ff3a1ef09f7
Bison Zhang <bisonzhang@gmail.com>
2013-06-24 13:36:45 +0800

    profile ui done

--- app/controllers/home_view_controller.rb
+++ app/controllers/home_view_controller.rb
@@ -6,23 +6,34 @@ class HomeViewController < UIViewController
     self.navigationController.navigationBar.setBackgroundImage('top.png'.uiimage, forBarMetrics:UIBarMetricsDefault)
     #UIBarButtonItem.appearance.setBackButtonBackgroundImage('navi_back.png'.uiimage, forState: UIControlStateNormal, barMetrics: UIBarMetricsDefault)
     #UIBarButtonItem.appearance.setTitle('返回')
-
-    @menu = QuadCurveMenu.alloc.initWithFrame([[100,260],[210,210]], withArray:['1', '2', '3'])
+    menu_y = self.view.bounds.size.height - 230
+    @menu = QuadCurveMenu.alloc.initWithFrame([[150,menu_y],[210,210]], withArray:['1', '2', '3'])
     dir = QuadCurveRadialDirector.alloc.initWithMenuWholeAngle(HALF_PI, andInitialRotation:-1*HALF_PI)
     dir.endRadius = 100
     @menu.setMenuDirector(dir)
-    main_menu = QuadCurveDefaultMenuItemFactory.alloc
-      .initWithImage('bg-addbutton.png'.uiimage, highlightImage:'bg-addbutton-highlighted.png'.uiimage)
+    main_menu = QuadCurveDefaultMenuItemFactory.alloc.initWithImage('menu'.uiimage, highlightImage:nil)
     @menu.setMainMenuItemFactory(main_menu)
-    menu_item = QuadCurveDefaultMenuItemFactory.alloc
-      .initWithImage("bg-menuitem.png".uiimage,  highlightImage:"bg-menuitem-highlighted.png".uiimage)
-    @menu.setMenuItemFactory(menu_item)
+    @menu.setMenuItemFactory(self)
     self.view.addSubview @menu
     @menu.delegate = self
   end
 
+  IMGS = {'1'=>'menu-setting', '2'=>'menu-book', '3'=>'menu-event'}
+  def createMenuItemWithDataObject(data)
+    img = IMGS[data].uiimage
+    item = QuadCurveMenuItem.alloc.initWithImage(img, highlightedImage:nil)
+    item.dataObject = data
+    item
+  end
+
   def quadCurveMenu(menu, didTapMenuItem: item)
     puts "MenuItem tapped: #{item.dataObject}"
+    case item.dataObject
+      when '1'  #setting
+        self.performSegueWithIdentifier('ShowProfile', sender: self)
+      else
+        puts 'not implemented yet'
+    end
   end
 
 end
---
+++ app/controllers/profile_view_controller.rb
@@ -0,0 +1,3 @@
+class ProfileViewController < UIViewController
+
+end
