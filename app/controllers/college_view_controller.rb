328148b5f17d433ad7f9352d7b5fa080a44e349b
Bison Zhang <bisonzhang@gmail.com>
2013-07-01 22:44:08 +0800

    college view with a top image, tableview embeded

--- app/controllers/college_view_controller.rb
+++ app/controllers/college_view_controller.rb
@@ -1,18 +1,78 @@
-class CollegeViewController < Nitron::TableViewController
-  collection { College.all }
-
+class CollegeViewController < Nitron::ViewController
   def viewDidLoad
     super
+    tableView.dataSource = dataSource
 
-    view.addPullToRefreshWithActionHandler(lambda {
+    tableView.addPullToRefreshWithActionHandler(lambda {
       College.refresh
-      view.pullToRefreshView.stopAnimating
+      tableView.pullToRefreshView.stopAnimating
     })
 
     unless College.first
       puts 'No college data, pull to refresh...'
-      view.triggerPullToRefresh
+      tableView.triggerPullToRefresh
     end
   end
+
+  def controllerDidChangeContent(controller)
+    tableView.reloadData()
+  end
+
+  def dataSource
+    @_dataSource ||= begin
+      CoreDataSource.alloc.initWithRequest(College.all, owner:self)
+    end
+  end
+
+  def prepareForSegue(segue, sender:sender)
+    model = nil
+
+    if tableView.indexPathForSelectedRow
+      model = dataSource.objectAtIndexPath(tableView.indexPathForSelectedRow)
+    end
+
+    if model
+      controller = segue.destinationViewController
+      controller.model = model
+    end
+  end
+
+protected
+  class CoreDataSource
+    def initWithRequest(request, owner:owner)
+      if init
+        context = App.delegate.managedObjectContext
+
+        @controller = NSFetchedResultsController.alloc.initWithFetchRequest(request,
+                                                                            managedObjectContext:context,
+                                                                            sectionNameKeyPath:nil,
+                                                                            cacheName:nil)
+        @controller.delegate = owner
+
+        errorPtr = Pointer.new(:object)
+        unless @controller.performFetch(errorPtr)
+          raise "Error fetching data"
+        end
+      end
+
+      self
+    end
+
+    def objectAtIndexPath(indexPath)
+      @controller.objectAtIndexPath(indexPath)
+    end
+
+    def tableView(tview, cellForRowAtIndexPath:indexPath)
+      cell = tview.dequeueReusableCellWithIdentifier('CollegeCell')
+      model = objectAtIndexPath(indexPath)
+      Nitron::UI::DataBinder.shared.bind(model, cell)
+      cell
+    end
+
+    def tableView(tview, numberOfRowsInSection:section)
+      @controller.sections[section].numberOfObjects
+    end
+
+  end  # end of CoreDataSource
 
 end
--- app/controllers/course_detail_view_controller.rb
+++ app/controllers/course_detail_view_controller.rb
@@ -1,6 +1,6 @@
 class CourseDetailViewController < UIViewController
   attr_accessor :scroller
   def viewDidLoad
-    self.scroller.contentSize = CGSizeMake(320, 1200)
+    self.scroller.contentSize = CGSizeMake(320, 800)
   end
 end
